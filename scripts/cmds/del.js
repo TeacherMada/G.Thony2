054d08b062d00ae6b6dbf8188beda1ca786880da
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-03-28 13:18:38 +0300

    Create del.js

---
+++ scripts/cmds/del.js
@@ -0,0 +1,44 @@
+#cmd install del.js module.exports = {
+  config: {
+    name: "del",
+    aliases: ["delete"],
+    author: "S",
+role: 2,
+shortDescription: {
+  en:"[👨‍🔧] Admin cmd"
+},
+
+    category: "Admin"
+  },
+
+
+  onStart: async function ({ api, event, args }) {
+   const permission = ["61552825191002"];
+ if (!permission.includes(event.senderID))
+ return api.sendMessage("⚠️ | Seul nos développeurs peuvent utiliser ce commande", event.threadID, event.messageID);
+    const fs = require('fs');
+    const path = require('path');
+
+
+    const fileName = args[0];
+
+
+    if (!fileName) {
+      api.sendMessage("Please provide a filname to del.", event.threadID);
+      return;
+    }
+
+
+    const filePath = path.join(__dirname, fileName);
+
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error(err);
+        api.sendMessage(`❎ | Failed to delete ${fileName}.`, event.threadID);
+        return;
+      }
+      api.sendMessage(`✅ ( ${fileName} ) Deleted successfully!`, event.threadID);
+    });
+  }
+};
