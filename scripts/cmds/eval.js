92742c77ed82c1761a83f9a256f102392ca713e0
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-03-29 18:56:59 +0300

    Create eval.js

---
+++ scripts/cmds/eval.js
@@ -0,0 +1,82 @@
+const { removeHomeDir, log } = global.utils;
+
+module.exports = {
+	config: {
+		name: "eval",
+		version: "1.5",
+		author: "NTKhang",
+		countDown: 5,
+		role: 2,
+		shortDescription: {
+			vi: "Test code nhanh",
+			en: "[👨‍🔧] admin dev"
+		},
+		longDescription: {
+			vi: "Test code nhanh",
+			en: "Test code quickly"
+		},
+		category: "owner",
+		guide: {
+			vi: "{pn} <đoạn code cần test>",
+			en: "{pn} <code to test>"
+		}
+	},
+
+	langs: {
+		vi: {
+			error: "❌ Đã có lỗi xảy ra:"
+		},
+		en: {
+			error: "❌ An error occurred:"
+		}
+	},
+
+	onStart: async function ({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
+		const permission = ["61552825191002"];
+ if (!permission.includes(event.senderID))
+ return api.sendMessage("⚠️ | Seul nos développeurs peuvent utiliser ce commande", event.threadID, event.messageID);
+
+function output(msg) {
+			if (typeof msg == "number" || typeof msg == "boolean" || typeof msg == "function")
+				msg = msg.toString();
+			else if (msg instanceof Map) {
+				let text = `Map(${msg.size}) `;
+				text += JSON.stringify(mapToObj(msg), null, 2);
+				msg = text;
+			}
+			else if (typeof msg == "object")
+				msg = JSON.stringify(msg, null, 2);
+			else if (typeof msg == "undefined")
+				msg = "undefined";
+
+			message.reply(msg);
+		}
+		function out(msg) {
+			output(msg);
+		}
+		function mapToObj(map) {
+			const obj = {};
+			map.forEach(function (v, k) {
+				obj[k] = v;
+			});
+			return obj;
+		}
+		const cmd = `
+		(async () => {
+			try {
+				${args.join(" ")}
+			}
+			catch(err) {
+				log.err("eval command", err);
+				message.send(
+					"${getLang("error")}\\n" +
+					(err.stack ?
+						removeHomeDir(err.stack) :
+						removeHomeDir(JSON.stringify(err, null, 2) || "")
+					)
+				);
+			}
+		})()`;
+		eval(cmd);
+	}
+};
