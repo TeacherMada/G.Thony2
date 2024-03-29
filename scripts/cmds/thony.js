f95c38324a2038bc82b5ae299c18034521f9c5ff
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-03-29 15:55:50 +0300

    Update thony.js

--- scripts/cmds/thony.js
+++ scripts/cmds/thony.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
+
 module.exports = {
   config: {
     name: 'thony',
@@ -18,30 +19,36 @@ module.exports = {
     },
   },
 
+
   onStart: async function ({ api, event, args, usersData }) {
     try {
       const query = args.join(" ") || "Bonjour";
       const { name } = (await usersData.get(event.senderID));
 
+
       if (query) {
-        api.setMessageReaction("⏳", event.messageID, (err) =&gt; console.log(err), true);
+        api.setMessageReaction("⏳", event.messageID, (err) => console.log(err), true); // Changed '&gt;' to '>'
         const processingMessage = await api.sendMessage(
           `Attendez svp...`,
           event.threadID
         );
 
-        const apiUrl = `https://lianeapi.onrender.com/@unregistered/api/thon?key=j86bwkwo-8hako-12C&amp;userName=${encodeURIComponent(name || "a user")}&amp;query=${encodeURIComponent(query)}`;
+
+        const apiUrl = `https://lianeapi.onrender.com/@hercai/api/Herc.ai?key=j86bwkwo-8hako-12C&userName=${encodeURIComponent(name || "a user")}&query=${encodeURIComponent(query)}`; // Changed '&amp;' to '&'
         const response = await axios.get(apiUrl);
 
-        if (response.data &amp;&amp; response.data.message) {
+
+        if (response.data && response.data.message) { // Changed '&amp;&amp;' to '&&'
           const trimmedMessage = response.data.message.trim();
-          api.setMessageReaction("✅", event.messageID, (err) =&gt; console.log(err), true);
+          api.setMessageReaction("✅", event.messageID, (err) => console.log(err), true); // Changed '&gt;' to '>'
           await api.sendMessage({ body: trimmedMessage }, event.threadID, event.messageID);
+
 
           console.log(`Sent Thony's response to the user`);
         } else {
           throw new Error(`Invalid or #thony question`);
         }
+
 
         await api.unsendMessage(processingMessage.messageID);
       }
