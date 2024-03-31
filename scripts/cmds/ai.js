2e3d357169ff8d8c1cf682e6304baa3923d0cd0d
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-03-31 10:28:11 +0300

    Create ai.js

---
+++ scripts/cmds/ai.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+async function a(api, event, args, message) {
+  try {
+    const a = args.join(" ").trim();
+
+    if (!a) {
+      return message.reply("Ex: #ai Salut");
+    }
+
+    const b = "Your name is Tsanta who lives in Madagascar Antananarivo, Specialist creation Chatbot,  contact: 0349310268 if anyone want to create new Chatbot Facebook. you're helpful"; // the more better content you give the  best it became
+    const c = await d(a, b);
+
+    if (c.code === 2 && c.message === "success") {
+      message.reply(c.answer, (r, s) => {
+        global.GoatBot.onReply.set(s.messageID, {
+          commandName: module.exports.config.name,
+          uid: event.senderID 
+        });
+      });
+    } else {
+      message.reply("Please try again later.");
+    }
+  } catch (e) {
+    console.error("Error:", e);
+    message.reply("An error occurred while processing your request.");
+  }
+}
+
+async function d(a, b) {
+  try {
+    const d = await axios.get(`https://personal-ai-phi.vercel.app/kshitiz?prompt=${encodeURIComponent(a)}&content=${encodeURIComponent(b)}`);
+    return d.data;
+  } catch (f) {
+    console.error("Error from api", f.message);
+    throw f;
+  }
+}
+
+module.exports = {
+  config: {
+    name: "Ai",// add your ai name here
+    version: "1.0",
+    author: "Tsanta",
+    role: 0,
+    longDescription: "[👑] Ai intelligent",// ai description
+    category: "ai",
+    guide: {
+      en: "{p}cmdName [prompt]"// add guide based on your ai name
+    }
+  },
+  
+  handleCommand: a,
+  onStart: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  },
+  onReply: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  }
+};
