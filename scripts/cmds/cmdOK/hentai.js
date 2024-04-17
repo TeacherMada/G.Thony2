754dfeb8300b984cfa3fce6eccb658afec264767
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2024-04-17 11:21:18 +0300

    Create hentai.js

---
+++ scripts/cmds/cmdOK/hentai.js
@@ -0,0 +1,79 @@
+function _0x1079() {
+  const _0x58815d = ['parse', 'utf8', '3kGTrgb', '327716QBhdvG', 'https://drive.google.com/uc?export=download&id=1AThVnoZc3X0Om4EL_vL7f8J0Kz8OF_mW', 'floor', 'filter', 'https://drive.google.com/uc?export=download&id=18yFGQ7dOyl-elkxVTKXyWVWnJpiuGuj4', 'push', '{p}hgif', 'https://drive.google.com/uc?export=download&id=194S4w7MQqIIbDemeY937haf18JwH-U9w', '5LHItXY', 'vip.json', 'https://drive.google.com/uc?export=download&id=18UiqHnWxJLRe1eFzO2ry_GBfhKzQ_dpf', '86471eIhgYH', '64652ivgmvn', '448apFsIR', 'path', 'https://drive.google.com/uc?export=download&id=19MkQq_BzbU2ipAWQTyFOvorje63-71W7', '1628014ubAqfV', 'https://drive.google.com/uc?export=download&id=1AfeMFXSf_VVDmo-sDag5r5_fmwHx_5G2', 'https://drive.google.com/uc?export=download&id=19fbomSh_ahuFalxi8dI_yr3NwPQ9Brq-', 'unsendMessage', 'https://drive.google.com/uc?export=download&id=19kRu8YKvWJn9OgKYrr1StoPJARV5Rcho', '5359098AWsnMs', "Wait a second master ʘ⁠‿⁠ʘ", 'https://drive.google.com/uc?export=download&id=18bhagyzyhiG7oZ_XrvdXjs4brShTw5Qj', 'hentaigif', 'senderID', "nhentai gif (⁠≧⁠▽⁠≦⁠)", 'hgif', 'join', "it will send hentai gif", 'messageID', 'https://drive.google.com/uc?export=download&id=1APB42F4G2XxVZvWJocybbyJJG4Ag8KXf', 'utils', 'random', 'https://drive.google.com/uc?export=download&id=1A2QYHGIiPIbmZ0YA63kZnQmhlqEryKh-', 'sentVideos', 'https://drive.google.com/uc?export=download&id=18cj5lyFFNp5D4i0nIR1xeFV0wYkQjATX', '36PbwrlW', '1.0', 'https://drive.google.com/uc?export=download&id=18pqO83sNR7guRDwNcxMkY-Knn2ST0xUM', "You are not a VIP member, contact admin(s) to access VIP command's.", 'includes', 'https://drive.google.com/uc?export=download&id=1AbPiHq4JEB6q9upIE61b1K55ifq9yAAS', '7157527JfEVcA', 'https://drive.google.com/uc?export=download&id=1ACYKwsTUV_xy93ndm9aArcO_bHrFAUJr', 'reply', 'https://drive.google.com/uc?export=download&id=1ATY940ULRWOF1ZR3KVBpQyRA2dFntGoa', 'getStreamFromURL', '1WXBScl', "18+ VIP", '1706571sxOIAQ', 'https://drive.google.com/uc?export=download&id=19oawVpXQlP5H9hhi07xldGFs0fvWhOP-', '930FzobjH', 'https://drive.google.com/uc?export=download&id=19avECf41haFwjiOt7AhBs1zp3uSksWbJ', 'https://drive.google.com/uc?export=download&id=1AU09od83l8LbD8v5aK3lH3IWGZ56SO_u', 'https://drive.google.com/uc?export=download&id=18cNaTWUEwTxWjzs7KY6OLtAHY9t2uuVD'];
+  _0x1079 = function () {
+    return _0x58815d;
+  };
+  return _0x1079();
+}
+const _0x273c53 = _0x3708;
+(function (_0x47a935, _0x420cac) {
+  const _0x38749e = _0x3708;
+  const _0x50b5aa = _0x47a935();
+  while (true) {
+    try {
+      const _0x5e06df = -parseInt(_0x38749e(0xa2)) / 0x1 * (parseInt(_0x38749e(0x82)) / 0x2) + -parseInt(_0x38749e(0x71)) / 0x3 * (-parseInt(_0x38749e(0x72)) / 0x4) + -parseInt(_0x38749e(0x7a)) / 0x5 * (-parseInt(_0x38749e(0x87)) / 0x6) + -parseInt(_0x38749e(0x7e)) / 0x7 * (parseInt(_0x38749e(0x7f)) / 0x8) + parseInt(_0x38749e(0xa4)) / 0x9 + -parseInt(_0x38749e(0xa6)) / 0xa * (parseInt(_0x38749e(0x7d)) / 0xb) + parseInt(_0x38749e(0x97)) / 0xc * (parseInt(_0x38749e(0x9d)) / 0xd);
+      if (_0x5e06df === _0x420cac) {
+        break;
+      } else {
+        _0x50b5aa.push(_0x50b5aa.shift());
+      }
+    } catch (_0x261c42) {
+      _0x50b5aa.push(_0x50b5aa.shift());
+    }
+  }
+})(_0x1079, 0xb81fc);
+function _0x3708(_0x290332, _0x5e2f40) {
+  const _0x10794a = _0x1079();
+  _0x3708 = function (_0x370810, _0x59fc31) {
+    _0x370810 = _0x370810 - 0x70;
+    let _0x549793 = _0x10794a[_0x370810];
+    return _0x549793;
+  };
+  return _0x3708(_0x290332, _0x5e2f40);
+}
+const fs = require('fs');
+const path = require(_0x273c53(0x80));
+module.exports = {
+  config: {
+    name: 'hentai',
+    aliases: ['hantai'],
+    version: '1.0',
+    author: 'tsanta',
+    countDown: 120,
+    role: 2,
+    shortDescription: "[👑] xx dessinAnimé hentai video",
+    longDescription: _0x273c53(0x8f),
+    category: _0x273c53(0xa3),
+    guide: _0x273c53(0x78)
+  },
+  'sentVideos': [],
+  'onStart': async function ({
+    api: _0x10bf64,
+    event: _0x41d4c8,
+    args: _0x9c0cfc,
+    message: _0x120599
+  }) {
+   
+    const _0x425753 = _0x41d4c8[_0x273c53(0x8b)];
+    const _0x2e50f4 = await _0x120599[_0x273c53(0x9f)]({
+      'body': _0x273c53(0x88)
+    });
+    const _0x23a8cd = [_0x273c53(0x7c), _0x273c53(0x89), _0x273c53(0xa9), _0x273c53(0x96), _0x273c53(0x99), _0x273c53(0x76), _0x273c53(0x79), 'https://drive.google.com/uc?export=download&id=18uaWYPNlxtgXV5vTrWdff-wXdQIcXV7S', 'https://drive.google.com/uc?export=download&id=19Kk2y6uG8gv4qFIu5SqRe_XQ1xDFKcGX', _0x273c53(0x81), 'https://drive.google.com/uc?export=download&id=19_EcrlOSXMEuMon7dnIKoNtwYhqBkPmb', _0x273c53(0xa7), _0x273c53(0x84), _0x273c53(0x86), 'https://drive.google.com/uc?export=download&id=19m11leSFQBm1IZXUDnZlPpnjFXGiNf1J', _0x273c53(0xa5), _0x273c53(0x94), 'https://drive.google.com/uc?export=download&id=1A4O3lm3QoRvJnZwX7rFexxYQp2XwXhmg', 'https://drive.google.com/uc?export=download&id=1A3771qz8HKYGXkw803sQo4qBS-YrQ5ft', _0x273c53(0x9e), _0x273c53(0x91), _0x273c53(0xa0), _0x273c53(0x73), _0x273c53(0xa8), 'https://drive.google.com/uc?export=download&id=1AVqRA2rNOZnq_Sv1gRIVEcRxnZqtna9W', _0x273c53(0x9c), _0x273c53(0x83)];
+    const _0x30d0e6 = _0x23a8cd[_0x273c53(0x75)](_0x391636 => !this.sentVideos[_0x273c53(0x9b)](_0x391636));
+    if (_0x30d0e6.length === 0x0) {
+      this.sentVideos = [];
+    }
+    const _0x4a85b5 = Math[_0x273c53(0x74)](Math[_0x273c53(0x93)]() * _0x30d0e6.length);
+    const _0x2337c7 = _0x30d0e6[_0x4a85b5];
+    this[_0x273c53(0x95)][_0x273c53(0x77)](_0x2337c7);
+    if (_0x425753 !== null) {
+      _0x120599.reply({
+        'body': _0x273c53(0x8c),
+        'attachment': await global[_0x273c53(0x92)][_0x273c53(0xa1)](_0x2337c7)
+      });
+      setTimeout(() => {
+        _0x10bf64[_0x273c53(0x85)](_0x2e50f4[_0x273c53(0x90)]);
+      }, 0x1388);
+    }
+  }
+};
